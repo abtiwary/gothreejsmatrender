1dd35f80a6ad3d91042edf3bcfb4592de56ee0c3
abtiwary <ab.tiwary@gmail.com>
2021-09-26 14:50:59 +1000

    arrow keys can now be used to rotate the teapot; added command-line argument support to the go server;

--- main.go
+++ main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"path/filepath"
 
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	fsServe := http.FileServer(http.Dir("/Users/abtiwary/Development/Golang/GoThreeJS/serve"))
-	fsStatic := http.FileServer(http.Dir("/Users/abtiwary/Development/Golang/GoThreeJS/serve/static"))
+	serverIP := flag.String("server_ip", "0.0.0.0", "the IP to serve on")
+	serverPort := flag.Int("server_port", 8088, "the Port to listen on")
+	dirToServe := flag.String("serve_dir", "./serve", "the directory to serve")
+	flag.Parse()
+
+	serverDir := *dirToServe
+	staticDir := filepath.Join(serverDir, "/static/")
+	fsServe := http.FileServer(http.Dir(serverDir))
+	fsStatic := http.FileServer(http.Dir(staticDir))
 
 	http.Handle("/", fsServe)
 	http.Handle("/static/", http.StripPrefix("/static/", fsStatic))
 
-	serverIP := "0.0.0.0"
-	serverPort := 8088
 	log.WithFields(log.Fields{
-		"server_ip": serverIP,
-		"server_port": serverPort,
+		"server_ip":   *serverIP,
+		"server_port": *serverPort,
 	}).Info("server is now listening")
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", serverIP, serverPort), nil)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", *serverIP, *serverPort), nil)
 	if err != nil {
 		log.WithError(err).Fatal("an error occurred")
 	}
